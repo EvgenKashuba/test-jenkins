200900ee0e82b1bea44d51f1a391e2bd46e059ce
Evgen <109951576+EvgenKashuba@users.noreply.github.com>
2023-01-23 19:13:05 +0300

    delete \ near variables

--- script.sh
+++ script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 FILENAME="log"
-CHOICE=\$1
-case \$CHOICE in
+CHOICE=$1
+case $CHOICE in
   "Jan" )
-   journalctl -p err | grep "\$CHOICE" > \$FILENAME-\$CHOICE-Error-\`date +%b\-%d\-%Y\-%T\` ;;
+   journalctl -p err | grep "$CHOICE" > $FILENAME-$CHOICE-Error-`date +%b\-%d\-%Y\-%T` ;;
   "Dec" )
-   journalctl -p err | grep "\$CHOICE" > \$FILENAME-\$CHOICE-Error-\`date +%b\-%d\-%Y\-%T\` ;;
+   journalctl -p err | grep "$CHOICE" > $FILENAME-$CHOICE-Error-`date +%b\-%d\-%Y\-%T` ;;
   "Nov" )
-   journalctl -p err | grep "\$CHOICE" > \$FILENAME-\$CHOICE-Error-\`date +%b\-%d\-%Y\-%T\` ;;
+   journalctl -p err | grep "$CHOICE" > $FILENAME-$CHOICE-Error-`date +%b\-%d\-%Y\-%T` ;;
   * )
-   journalctl -p err | tail -n 20 > \$FILENAME-Err-last20-\`date +%b\-%d\-%Y\-%T\`
+   journalctl -p err | tail -n 20 > $FILENAME-Err-last20-`date +%b\-%d\-%Y\-%T`
    CHOICE="this month (last 20)" 
    touch test ;;
 esac
-echo "ErrorLogs in \$CHOICE add in \$FILENAME succesful!"
+echo "ErrorLogs in $CHOICE add in $FILENAME succesful!"
